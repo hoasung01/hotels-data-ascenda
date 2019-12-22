bfe8fa516e216281aa93e59784a9e2b59536cc9c
hainguyen <hai@iwa.fi>
2019-12-22 10:14:53 +0700

    update call action

--- app/services/hotels/fetch_data_service.rb
+++ app/services/hotels/fetch_data_service.rb
@@ -1,4 +1,21 @@
 module Hotels
   class FetchDataService
+    TIMEOUT = 10
+
+    def initialize(supplier_url)
+      @supplier_url = supplier_url
+    end
+
+    def call
+      response = RestClient::Request.execute(
+        method: :get,
+        url: @supplier_url,
+        timeout: TIMEOUT
+      )
+      JSON.parse(response.body)
+    rescue RestClient::ExceptionWithResponse => e
+      Rails.logger.error("===Can not get data from #{@supplier_url} due to: #{e}===")
+      []
+    end
   end
 end
