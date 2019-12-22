a80d17193c10b87b20db4b545fc4c418c265aa45
hainguyen <hai@iwa.fi>
2019-12-22 13:07:07 +0700

    add attr_reader and expose them

--- app/services/hotels/fetch_data_service.rb
+++ app/services/hotels/fetch_data_service.rb
@@ -1,20 +1,22 @@
 module Hotels
   class FetchDataService
+    attr_reader :response, :error
     TIMEOUT = 10
 
-    def initialize(supplier_url)
+    def initialize(supplier_url:)
       @supplier_url = supplier_url
     end
 
     def call
-      response = RestClient::Request.execute(
+      @response = RestClient::Request.execute(
         method: :get,
         url: @supplier_url,
         timeout: TIMEOUT
       )
-      JSON.parse(response.body)
+      JSON.parse(@response.body)
     rescue RestClient::ExceptionWithResponse => e
       Rails.logger.error("===Can not get data from #{@supplier_url} due to: #{e}===")
+      @error = e
       []
     end
   end
