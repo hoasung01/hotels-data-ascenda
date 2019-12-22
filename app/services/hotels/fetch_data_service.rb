8797a7a17f908429a601e456d1f092ee9008dbdd
hainguyen <hai@iwa.fi>
2019-12-22 16:48:15 +0700

    update fetch_data_service to expose attr_reader

--- app/services/hotels/fetch_data_service.rb
+++ app/services/hotels/fetch_data_service.rb
@@ -8,16 +8,19 @@ module Hotels
     end
 
     def call
-      @response = RestClient::Request.execute(
-        method: :get,
-        url: @supplier_url,
-        timeout: TIMEOUT
-      )
-      JSON.parse(@response.body)
-    rescue RestClient::ExceptionWithResponse => e
-      Rails.logger.error("===Can not get data from #{@supplier_url} due to: #{e}===")
-      @error = e
-      []
+      begin
+        @response = RestClient::Request.execute(
+          method: :get,
+          url: @supplier_url,
+          timeout: TIMEOUT
+        )
+        JSON.parse(@response.body)
+      rescue RestClient::ExceptionWithResponse => e
+        Rails.logger.error("===Can not get data from #{@supplier_url} due to: #{e}===")
+        @error = e
+        []
+      end
+      self
     end
   end
 end
