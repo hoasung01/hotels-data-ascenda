991e2f5256393a1952770640909748a024b421fa
hainguyen <hai@iwa.fi>
2019-12-22 16:48:29 +0700

    update suppliers_constant to hash data

--- app/constants/suppliers_constant.rb
+++ app/constants/suppliers_constant.rb
@@ -1,7 +1,7 @@
 class SuppliersConstant
-  SUPPLIERS = %w(
-    https://api.myjson.com/bins/gdmqa
-    https://api.myjson.com/bins/1fva3m
-    https://api.myjson.com/bins/j6kzm
-  ).freeze
+  SUPPLIERS = {
+    bear: 'https://api.myjson.com/bins/gdmqa',
+    fish: 'https://api.myjson.com/bins/1fva3m',
+    dragon: 'https://api.myjson.com/bins/j6kzm'
+  }.freeze
 end
