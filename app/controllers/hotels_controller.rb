21d1c106077e937da96895cb3dc3fe7d2950d8cb
hainguyen <hai@iwa.fi>
2019-12-22 20:27:06 +0700

    updated filtered hotels response

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,6 +1,23 @@
 class HotelsController < ApplicationController
   def index
     response = Hotels::ParserDataService.new().()
-    render json: response.hotels
+    render json: filtered_for(response.hotels)
+  end
+
+  private
+
+  def filtered_for(hotels)
+    if params[:hotel_ids].present?
+      hotels_ids = params[:hotel_ids].split(',')
+
+      hotels = hotels.select do |hotel|
+        hotels_ids.include?(hotel[:id])
+      end
+    elsif params[:dest_id].present?
+      hotels = hotels.select do |hotel|
+        hotel[:destination_id] == params[:dest_id].to_i
+      end
+    end
+    hotels
   end
 end
