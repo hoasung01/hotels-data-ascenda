f73b5f672fd8f3a8f82e6578451553db2c17250a
hainguyen <hai@iwa.fi>
2019-12-22 19:12:11 +0700

    update categorized_data_service return more fields

--- app/services/hotels/categorized_data_service.rb
+++ app/services/hotels/categorized_data_service.rb
@@ -10,26 +10,47 @@ module Hotels
     def call
       case @supplier
       when :bear
-        @hotel = Hotel.new(
-          @payload['Id'],
-          @payload['DestinationId'],
-          @payload['Name'],
-          @payload['Description']
-        )
+        @hotel = Hotel.new.tap do |hotel|
+          hotel.id = @payload['Id']
+          hotel.destination_id = @payload['DestinationId']
+          hotel.name = @payload['Name']
+          hotel.description = @payload['Description']
+          hotel.location = {
+            lat: @payload['Latitude'],
+            lng: @payload['Longitude'],
+            address: @payload['Address'],
+            city: @payload['City'],
+            country: @payload['Country']
+          }
+        end.marshal_dump
       when :fish
-        @hotel = Hotel.new(
-          @payload['hotel_id'],
-          @payload['destination_id'],
-          @payload['hotel_name'],
-          @payload['details']
-        )
+        @hotel = Hotel.new.tap do |hotel|
+          hotel.id = @payload['hotel_id']
+          hotel.destination_id = @payload['destination_id']
+          hotel.name = @payload['hotel_name']
+          hotel.description = @payload['details']
+          hotel.location = {
+            lat: '',
+            lng: '',
+            address: @payload['location']['address'],
+            city: '',
+            country: @payload['location']['country']
+          }
+        end.marshal_dump
       when :dragon
-        @hotel = Hotel.new(
-          @payload['id'],
-          @payload['destination'],
-          @payload['name'],
-          @payload['info']
-        )
+        @hotel = Hotel.new.tap do |hotel|
+          hotel.id = @payload['id']
+          hotel.destination_id = @payload['destination']
+          hotel.name = @payload['name']
+          hotel.description = @payload['info']
+          hotel.location = {
+            lat: @payload['lat'],
+            lng: @payload['lng'],
+            address: @payload['address'],
+            city: '',
+            country: ''
+          }
+        end.marshal_dump
       end
       self
     end
