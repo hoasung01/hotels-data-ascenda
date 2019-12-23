0c05fba992e628776bd9d11b1a1c7d94bed9e84c
hainguyen <hai@iwa.fi>
2019-12-23 23:53:50 +0700

    add hotels_controller_test

---
+++ test/controllers/hotels_controller_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class HotelsControllerTest < ActionController::TestCase
+  def test_index
+  end
+
+  def test_index_with_hotel_ids_params
+  end
+
+  def test_index_with_dest_id_params
+  end
+
+  def test_index_with_hotel_ids_and_dest_id_params
+  end
+end
