f7a2a5a6eb67007099a455e30ca6220a46de0bfd
hainguyen <hai@iwa.fi>
2019-12-24 13:30:45 +0700

    refactor data_rule_service

--- app/services/hotels/data_rule_service.rb
+++ app/services/hotels/data_rule_service.rb
@@ -6,42 +6,29 @@ module Hotels
     end
 
     def call
-      if @dest[:location][:lat].blank?
-        @dest[:location][:lat] = @src[:location][:lat]
+      @dest.each do |key, value|
+        if value.is_a?(String)
+          if value.blank?
+            @dest[key] = @src[key]
+          end
+        elsif value.is_a?(Array)
+          if value.empty?
+            @dest[key] = @src[key]
+          end
+        elsif value.is_a?(Hash)
+          value.keys.each do |nested_key|
+            if value[nested_key].is_a?(String)
+              if @dest[key][nested_key].blank?
+                @dest[key][nested_key] = @src[key][nested_key
+              end
+            elsif value[nested_key].is_a?(Array)
+              if @dest[key][nested_key].empty?
+                @dest[key][nested_key] = @src[key][nested_key]
+              end
+            end
+          end
+        end
       end
-
-      if @dest[:location][:lng].blank?
-        @dest[:location][:lng] = @src[:location][:lng]
-      end
-
-      if @dest[:location][:address].blank?
-        @dest[:location][:address] = @src[:location][:address]
-      end
-
-      if @dest[:location][:city].blank?
-        @dest[:location][:city] = @src[:location][:city]
-      end
-
-      if @dest[:location][:country].blank?
-        @dest[:location][:country] = @src[:location][:country]
-      end
-
-      if @dest[:images][:rooms].blank?
-        @dest[:images][:rooms] = @src[:images][:rooms]
-      end
-
-      if @dest[:images][:site].blank?
-        @dest[:images][:site] = @src[:images][:site]
-      end
-
-      if @dest[:images][:amenities].blank?
-        @dest[:images][:amenities] = @src[:images][:amenities]
-      end
-
-      if @dest[:booking_conditions].blank?
-        @dest[:booking_conditions]= @src[:booking_conditions]
-      end
-
       @dest
     end
   end
