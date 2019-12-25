17f8dda05aba93858923aa6f19adeaa33e160437
hainguyen <hai@iwa.fi>
2019-12-25 10:15:49 +0700

    update bear_hotel_presenter_test

--- test/presenters/bear_hotel_presenter_test.rb
+++ test/presenters/bear_hotel_presenter_test.rb
@@ -2,18 +2,19 @@ require 'test_helper'
 
 class BearHotelPresenterTest < ActiveSupport::TestCase
   def test_presenter_bear_data
-    payload = File.read(File.join(Rails.root, 'test', 'fixtures', 'bear.json'))
+    body = File.read(File.join(Rails.root, 'test', 'fixtures', 'bear.json'))
+    payload = JSON.parse(body)[0]
     bear_presenter = BearHotelPresenter.new(payload: payload).as_json
 
-    assert_match('Id', bear_presenter[:id])
-    assert_match('DestinationId', bear_presenter[:destination_id])
-    assert_match('Name', bear_presenter[:name])
-    assert_match('Latitude', bear_presenter[:location][:lat])
-    assert_match('Longitude', bear_presenter[:location][:lng])
-    assert_match('Address', bear_presenter[:location][:address])
-    assert_match('City', bear_presenter[:location][:city])
-    assert_match('Country', bear_presenter[:location][:country])
-    assert_match('Description', bear_presenter[:description])
-    assert_match('Facilities', bear_presenter[:amenities][:general])
+    assert_same(payload['Id'], bear_presenter[:id])
+    assert_same(payload['DestinationId'], bear_presenter[:destination_id])
+    assert_same(payload['Name'], bear_presenter[:name])
+    assert_same(payload['Latitude'], bear_presenter[:location][:lat])
+    assert_same(payload['Longitude'], bear_presenter[:location][:lng])
+    assert_same(payload['Address'], bear_presenter[:location][:address])
+    assert_same(payload['City'], bear_presenter[:location][:city])
+    assert_same(payload['Country'], bear_presenter[:location][:country])
+    assert_same(payload['Description'], bear_presenter[:description])
+    assert_same(payload['Facilities'], bear_presenter[:amenities][:general])
   end
 end
